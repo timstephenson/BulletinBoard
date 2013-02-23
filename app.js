17a2bcc941c7aa0612eb92789f3820be765c5913
Tim Stephenson <tim@raddonline.com>
2013-02-22 16:27:03 -0800

    Initial Commit.

---
+++ app.js
@@ -0,0 +1,32 @@
+// A.1
+var express = require('express'),
+    app = express();
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+ 
+// A.2
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+});
+ 
+// A.3
+server.listen(1337);
+
+// Configure io callbacks.
+io.sockets.on('connection', function(socket) {
+    socket.on('createNote', function(data) {
+        socket.broadcast.emit('onNoteCreated', data);
+    });
+ 
+    socket.on('updateNote', function(data) {
+        socket.broadcast.emit('onNoteUpdated', data);
+    });
+ 
+    socket.on('deleteNote', function(data){
+        socket.broadcast.emit('onNoteDeleted', data);
+    });
+ 
+    socket.on('moveNote', function(data){
+        socket.broadcast.emit('onNoteMoved', data);
+    });
+});
